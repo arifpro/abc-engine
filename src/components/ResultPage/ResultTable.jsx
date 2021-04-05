43ef1fcc3da78a27ff76d3739a245756790f21f4
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2021-04-05 22:23:21 +0800

    pdf generate

--- src/components/ResultPage/ResultTable.jsx
+++ src/components/ResultPage/ResultTable.jsx
@@ -1,53 +1,122 @@
+import { createRef } from 'react';
 import { connect } from 'react-redux';
+import Pdf from 'react-to-pdf';
 import styles from '../../styles/ResultPage/ResultTableStyles.module.scss';
 
-const ResultTable = ({ projectState }) => (
-    <main className={styles.resultTable}>
-        <section className={styles.tableSection}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>projectName</th>
-                        <th>projectDesc</th>
-                        <th>client</th>
-                        <th>contractor</th>
-                        <th>max_X</th>
-                        <th>min_X</th>
-                        <th>max_Y</th>
-                        <th>min_Y</th>
-                        <th>max_Z</th>
-                        <th>min_Z</th>
-                    </tr>
-                </thead>
+const ResultTable = ({ projectState }) => {
+    const ref = createRef();
 
-                <tbody>
-                    {projectState && (
+    return (
+        <main className={styles.resultTable}>
+            <Pdf targetRef={ref} filename="code-example.pdf">
+                {({ toPdf }) => (
+                    <button type="button" onClick={toPdf}>
+                        Generate Pdf
+                    </button>
+                )}
+            </Pdf>
+
+            {/* <section className={styles.tableSection} ref={ref}>
+                <table>
+                    <thead>
                         <tr>
+                            <th>projectName</th>
+                            <th>projectDesc</th>
+                            <th>client</th>
+                            <th>contractor</th>
+                            <th>max_X</th>
+                            <th>min_X</th>
+                            <th>max_Y</th>
+                            <th>min_Y</th>
+                            <th>max_Z</th>
+                            <th>min_Z</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {projectState && (
+                            <tr>
+                                <td>
+                                    {projectState.projectName?.length > 10
+                                        ? `${projectState.projectName.slice(0, 12)}...`
+                                        : projectState.projectName}
+                                </td>
+                                <td>
+                                    {projectState.projectDesc?.length > 10
+                                        ? `${projectState.projectDesc.slice(0, 12)}...`
+                                        : projectState.projectDesc}
+                                </td>
+                                <td>{projectState.client}</td>
+                                <td>{projectState.contractor}</td>
+                                <td>{projectState.maxX}</td>
+                                <td>{projectState.minX}</td>
+                                <td>{projectState.maxY}</td>
+                                <td>{projectState.minY}</td>
+                                <td>{projectState.maxZ}</td>
+                                <td>{projectState.minZ}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </section> */}
+
+            <section className={styles.tableSection2} ref={ref}>
+                {projectState && (
+                    <table>
+                        <tr>
+                            <th>projectName</th>
                             <td>
                                 {projectState.projectName?.length > 10
                                     ? `${projectState.projectName.slice(0, 12)}...`
                                     : projectState.projectName}
                             </td>
+                        </tr>
+                        <tr>
+                            <th>projectDesc</th>
                             <td>
                                 {projectState.projectDesc?.length > 10
                                     ? `${projectState.projectDesc.slice(0, 12)}...`
                                     : projectState.projectDesc}
                             </td>
+                        </tr>
+                        <tr>
+                            <th>client</th>
                             <td>{projectState.client}</td>
+                        </tr>
+                        <tr>
+                            <th>contractor</th>
                             <td>{projectState.contractor}</td>
+                        </tr>
+                        <tr>
+                            <th>max_X</th>
                             <td>{projectState.maxX}</td>
+                        </tr>
+                        <tr>
+                            <th>min_X</th>
                             <td>{projectState.minX}</td>
+                        </tr>
+                        <tr>
+                            <th>max_Y</th>
                             <td>{projectState.maxY}</td>
+                        </tr>
+                        <tr>
+                            <th>min_Y</th>
                             <td>{projectState.minY}</td>
+                        </tr>
+                        <tr>
+                            <th>max_Z</th>
                             <td>{projectState.maxZ}</td>
+                        </tr>
+                        <tr>
+                            <th>min_Z</th>
                             <td>{projectState.minZ}</td>
                         </tr>
-                    )}
-                </tbody>
-            </table>
-        </section>
-    </main>
-);
+                    </table>
+                )}
+            </section>
+        </main>
+    );
+};
 
 const mapStateToProps = (state) => ({
     projectState: state.project.projectData,
